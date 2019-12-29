3ee5bab8f86c6bd3a8f29829cfbaefd7f6c143a9
Sithija Shehara <nsithijashehara@gmail.com>
2019-12-29 23:13:16 +0530

    initial change - styling

---
+++ src/components/Tweet.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/TweetStyling.css"
+export default function Tweet(){
+    return(
+        <div class ="TweetScreen">
+            <h1>Tweet Component</h1>
+        </div>
+    );
+}
