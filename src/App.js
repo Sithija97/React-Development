caf365150d1c0143db5a5a0d27d3a02322bccb43
Sithija Shehara <nsithijashehara@gmail.com>
2020-01-05 19:43:29 +0530

    installed material ui and icons

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-
-import firebase from "firebase";
-import StyleFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-
-firebase.initializeApp({
-  apiKey: "  AIzaSyCBfn3CYHY-1UvF2yjoS_fyyLD65QjUtjk",
-  authDomain: "angular-authentication-a5ab0.firebaseapp.com"
-});
-
+import Auth from "./Auth";
+import Loading from "./Loading"
 class App extends Component {
-  state = { isSignedIn: false };
-  uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
-      
-    ],
-    callbacks: {
-      signInSuccess: () => false
-    }
-  };
-
-  componentDidMount = () => {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user });
-    });
-  };
-
   render() {
-    return (
-      <div className="App">
-        <h1>Sithija Shehara</h1>
-        {this.state.isSignedIn ? (
-          <div>
-            Signed In!
-            <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
-          </div>
-        ) : (
-          <StyleFirebaseAuth
-            className="SocialButtons"
-            uiConfig={this.uiConfig}
-            firebaseAuth={firebase.auth()}
-          />
-        )}
-      </div>
-    );
+    return (<Loading/>);
   }
 }
 
