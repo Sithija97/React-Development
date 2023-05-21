db2c029d9535fa573c11472e0c8ac5bb6bb105f1
Sithija Shehara <nsithijashehara@gmail.com>
2023-05-21 11:17:30 +0530

    added display author & timestamp

--- dave-redux-project/src/components/postList.jsx
+++ dave-redux-project/src/components/postList.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "../store/features/posts/postSlice";
+import PostAuthor from "./postAuthor";
+import TimeAgo from "./timeAgo";
 
 const PostList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post, id) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post, id) => (
     <article key={id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
     </article>
   ));
   return (
