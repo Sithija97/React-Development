bea12c1eedeb1359afc5fdf9040c2aa1ab4495c8
Sithija Shehara <nsithijashehara@gmail.com>
2023-06-19 08:48:31 +0530

    code change

--- react-konvajs-ts-app/src/store/features/shapes/shapeSlice.ts
+++ react-konvajs-ts-app/src/store/features/shapes/shapeSlice.ts
@@ -2,24 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // interface ExampleState {}
 
+const square = {
+  type: "square",
+  x: 20,
+  y: 20,
+  width: 50,
+  height: 50,
+  fill: "red",
+  draggable: true,
+};
+
 const initialState = {
-  shapes: [
-    {
-      type: "square",
-      x: 20,
-      y: 20,
-      width: 50,
-      height: 50,
-      fill: "red",
-      draggable: true,
-    },
-  ],
+  shapes: [],
 };
 
 const shapeSlice = createSlice({
   name: "shapes",
   initialState,
-  reducers: {},
+  reducers: {
+    addSquare(state, action) {},
+  },
 });
 
 // export const { exampleAction } = shapeSlice.actions;
