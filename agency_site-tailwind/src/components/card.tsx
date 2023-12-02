482990150304c7b180b908888d2a414579c2b7ae
Sithija Shehara <nsithijashehara@gmail.com>
2023-12-02 23:42:39 +0530

    courses UI - part 2

--- agency_site-tailwind/src/components/card.tsx
+++ agency_site-tailwind/src/components/card.tsx
@@ -1,20 +1,31 @@
-import { blogImg1 } from "../assets";
 import { Rating } from "./rating";
 
-export const Card = () => {
+export interface ICourse {
+  id: number;
+  title: string;
+  category: string;
+  rating: number;
+  price: string;
+  linkImg: string;
+}
+interface IProps {
+  course: ICourse;
+}
+
+export const Card = ({ course }: IProps) => {
   return (
     <div className="bg-white drop-shadow-md rounded-2xl md:mr-4 mr-2 my-4">
       <img
-        src={blogImg1}
+        src={course.linkImg}
         className="h-40 w-full object-cover overflow-hidden rounded-2xl"
       />
       <div className="p-5 border border-b">
-        <h1 className="py-2">programing concepts</h1>
-        <Rating rating={3} />
+        <h1 className="py-2">{course.title}</h1>
+        <Rating rating={course.rating} />
       </div>
-      <h3 className="p-5 text-xl">$500</h3>
+      <h3 className="p-5 text-xl">{course.price}</h3>
       <div className="absolute top-0 bg-white m-3 px-2 py-[2.5px] rounded font-bold">
-        HTML
+        {course.category}
       </div>
     </div>
   );
--- agency_site-tailwind/src/components/courses.tsx
+++ agency_site-tailwind/src/components/courses.tsx
@@ -1,5 +1,6 @@
 import { Card } from "./card";
 import Slider from "react-slick";
+import { courses } from "../data/cources";
 
 export const Courses = () => {
   const settings = {
@@ -38,13 +39,18 @@ export const Courses = () => {
     ],
   };
   return (
-    <div className="w-full bg-[#E9F8F3B2] py-24">
+    <div className="w-full bg-[#E9F8F3B2] py-32">
       <div className="md:max-w-[1480px] max-w-[600px] m-auto  px-4 md:px-0">
+        <h1 className="py-3 text-3xl font-bold text-[#21212F]">
+          Most Popular <span className="text-[#20B486]">Courses</span>
+        </h1>
+        <p className="text-[#6D737A]">
+          Various versions have evolved over the years, sometimes by accident,
+        </p>
         <Slider {...settings}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {courses.map((course) => (
+            <Card course={course} />
+          ))}
         </Slider>
       </div>
     </div>
